f46a0b87c3cd9670dc0ae20c2c8cd70ecc0beaa9
kajol <kajolkumarisingh222@gmail.com>
2023-06-09 17:20:41 +0530

    Update App.js

--- workshop-frontend/src/App.js
+++ workshop-frontend/src/App.js
@@ -6,17 +6,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src="./home_img.png" className="App-logo" alt="logo" />
-        <p>
-        <code>Welcome to API 2.0 Workshop!</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://studentsummit21.eventbrite.com/?aff=KK-44"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Register for Postman Student Summit!!
-        </a>
       </header>
       <body className="App-body">
         <DataList />
