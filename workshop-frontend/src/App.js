df663df013d3c999549ecd4c41a487e730510010
kajol <kajolkumarisingh222@gmail.com>
2024-04-14 15:48:02 +0530

    Update App.js

--- workshop-frontend/src/App.js
+++ workshop-frontend/src/App.js
@@ -6,6 +6,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src="./home_img.png" className="App-logo" alt="logo" />
+        <p>
+        <code>Welcome to API 2.0 Workshop!</code>
+        </p>
+        <a
+          className="App-link"
+          href="https://www.postman.com/student-program/student-leader/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Register for Postman Student Leader Program!!
+        </a>
       </header>
       <body className="App-body">
         <DataList />
